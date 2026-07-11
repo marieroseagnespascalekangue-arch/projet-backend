e02d2731dad26558c58bba383678266bc15dec87
Agnes-Pascale <marieroseagnespascalekangue@gmail.com>
2026-07-11 05:30:17 +0100

    fix: add Vercel URL to CORS

--- config/cors.php
+++ config/cors.php
@@ -6,11 +6,10 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:3001',
-        'http://127.0.0.1:3001',
-    ],
+       'allowed_origins' => [
+    'http://localhost:3000',
+    'https://projet-frontend-eight.vercel.app',
+],
 
     'allowed_origins_patterns' => [],
 
