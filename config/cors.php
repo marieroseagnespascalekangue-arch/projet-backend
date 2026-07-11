767c760f54b47e11b8028fd89832a53fb3d3a2e8
Agnes-Pascale <marieroseagnespascalekangue@gmail.com>
2026-07-11 06:06:35 +0100

    fix: correct cors.php syntax

--- config/cors.php
+++ config/cors.php
@@ -2,22 +2,14 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
-       'allowed_origins' => [
-    'http://localhost:3000',
-    'https://projet-frontend-eight.vercel.app',
-],
-
+        'http://localhost:3000',
+        'https://projet-frontend-eight.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
